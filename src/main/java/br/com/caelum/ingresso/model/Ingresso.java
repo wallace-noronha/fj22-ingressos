ca461bbaa709999a978f288ce8f7ce630c5007e5
Wallace <wallace@teste.com.br>
2018-11-24 10:10:19 -0200

    acertando detalhes

--- src/main/java/br/com/caelum/ingresso/model/Ingresso.java
+++ src/main/java/br/com/caelum/ingresso/model/Ingresso.java
@@ -10,6 +10,10 @@ public class Ingresso {
 	private Sessao sessao;
 	
 	private BigDecimal preco;
+	
+	private Lugar lugar;
+	
+	private TipoDeIngresso tipoDeIngresso;
 
 	/**
 	 * @deprecated hibernate only
@@ -17,9 +21,12 @@ public class Ingresso {
 	public Ingresso() {
 	}
 	
-	public Ingresso(Sessao sessao, Desconto tipoDeDesconto) {
+	public Ingresso(Sessao sessao, TipoDeIngresso tipoDeIngresso, Desconto tipoDeDesconto) {
 		this.sessao = sessao;
-		this.preco = tipoDeDesconto.aplicarDescontoSobre(sessao.getPreco());
+		this.tipoDeIngresso = tipoDeIngresso;
+		this.preco = this.tipoDeIngresso.aplicaDesconto(sessao.getPreco());
+
+		this.lugar = lugar;
 	}
 	
 	public BigDecimal getPreco() {
